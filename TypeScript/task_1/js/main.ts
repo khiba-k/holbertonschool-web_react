c717776f354dd588c39f8a40cd0977e4da0cfbab
khiba-k <khibakoenane@gmail.com>
2025-04-17 11:37:13 +0200

    task: Task2, created interface(Director) that extends the Teacher interface

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,31 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: string;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: "John",
+  fullTimeEmployee: false,
+  lastName: "Doe",
+  location: "London",
+  contract: false,
+};
+
+console.log("Teacher: ", teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log("Director: ", director1);
