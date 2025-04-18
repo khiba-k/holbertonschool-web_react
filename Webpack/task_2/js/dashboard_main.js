2d5cd3359ca7c5f27e96f2700676441e4d1b7c9f
khiba-k <khibakoenane@gmail.com>
2025-04-18 18:04:44 +0200

    task: Task2: Bundled images and css

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -4,7 +4,9 @@ const _ = require("lodash");
 $(document).ready(function () {
   $("head").append("<link rel='stylesheet' href='../css/main.css' />");
 
-  $("body").append("<div id='logo'></div>");
+  $("body").append(
+    '<div id="logo" style="width: 200px; height: 200px;"></div>'
+  );
   $("body").append("<p>Holberton Dashboard</p>");
   $("body").append("<p>Dashboard data for the students</p>");
   $("body").append("<button>Click here to get started</button>");
