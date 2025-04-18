31f139a0986b72d70983e0feab29e3d0daee926d
khiba-k <khibakoenane@gmail.com>
2025-04-18 14:55:01 +0200

    task: task1, created bundle for jquery counter code used lodash _debounce to slow down click functionality

--- Webpack/task_1/webpack.config.js
+++ Webpack/task_1/webpack.config.js
@@ -1,17 +1,10 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./js/dashboard_main.js",
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "main.js",
+    filename: "bundle.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html", // Your source HTML
-      filename: "index.html", // Output filename in the `public` folder
-    }),
-  ],
   mode: "production",
 };
