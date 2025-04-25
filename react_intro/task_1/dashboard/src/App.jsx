bb98893d7ee81c7a907df41702d7af9130dce0a6
khiba-k <khibakoenane@gmail.com>
2025-04-25 17:10:23 +0200

    Added div to form

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -18,13 +18,15 @@ function App() {
           Login to access the full dashboard
         </p>
 
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
 
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
 
-        <button>OK</button>
+          <button>OK</button>
+        </div>
       </div>
       <div className="App-footer">
         <p>
