15082373e3ebef32a6e1777385b6125847cc9652
khiba-k <khibakoenane@gmail.com>
2025-04-25 14:34:07 +0200

    task: Task1, Created component and utility functions for App.js

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import HolbertonLogo from "./assets/holberton-logo.jpg"
+import Notifications from './Notifications'
+import { getCurrentYear, getFooterCopy } from './utils'
+
+function App() {
+
+  return (
+    <>
+      <div className='root-notifications'>
+        <Notifications />
+      </div>
+      <div className='App-header' >
+        <img alt="holberton logo" src={HolbertonLogo} />
+        <h1>School Dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright {getCurrentYear()} {getFooterCopy()}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,9 @@
+const getCurrentYear = () => {
+  return "2025";
+};
+
+const getFooterCopy = (isIndex) => {
+  return isIndex ? "Holberton School" : "Holberton School main dashboard";
+};
+
+export { getCurrentYear, getFooterCopy };
