5e9acc4d14af0e25c249c3ec2d0154acd5bcf747
khiba-k <khibakoenane@gmail.com>
2025-04-26 19:13:10 +0200

    task: Created tests for Notifications component

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -5,16 +5,17 @@ import { getLatestNotification } from './utils';
 
 export default function Notifications() {
   return (
-    <div className="notifications"><div>
-      <p>Here is the list of notifications</p>
-      <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent">New resume available</li>
-        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
-      </ul>
-    </div>
-      <button alt="Close Button" onClick={() => { console.log("Close button has been clicked") }} style={{ marginLeft: "auto", backgroundColor: "white", border: "none", height: "25px" }} aria-label="Close">
-        <img src={closeIcon}></img>
+    <div className="notifications">
+      <div>
+        <p>Here is the list of notifications</p>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+        </ul>
+      </div>
+      <button aria-label="Close" onClick={() => { console.log("Close button has been clicked") }} style={{ marginLeft: "auto", backgroundColor: "white", border: "none", height: "25px" }}>
+        <img alt="Close Button" src={closeIcon}></img>
       </button>
 
     </div>
