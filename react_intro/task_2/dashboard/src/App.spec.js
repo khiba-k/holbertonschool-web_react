c3f98e8cb1c950ee4769c1423a881637013109ce
khiba-k <khibakoenane@gmail.com>
2025-04-26 09:21:53 +0200

    task: Task2, Created login form and test for it

---
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("h1 element with the text School Dashboard is rendered", () => {
+  render(<App />);
+  const heading = screen.getByRole("heading", { name: /School Dashboard/i });
+  expect(heading).toBeInTheDocument();
+});
+
+test("the text content within the 2 p elements in the app-body and app-footer divs matches", () => {
+  render(<App />);
+  const divbody = screen.getByText(/Login to access the full dashboard/i);
+  const divfooter = screen.getByText(/Copyright 2025 - holberton School/i);
+
+  expect(divbody).toBeInTheDocument();
+  expect(divfooter).toBeInTheDocument();
+});
+
+test("an img element is rendered", () => {
+  render(<App />);
+  const image = screen.getByAltText(/holberton logo/i);
+  expect(image).toBeInTheDocument();
+});
+
+test("renders two input elements", () => {
+  // Render component
+  render(<App />);
+
+  //Get input elements
+  const inputElements = screen.getAllByRole("textbox");
+  const inputLength = inputElements.length;
+
+  // Get labels
+  const email = screen.getByLabelText(/Email/i);
+  const password = screen.getByLabelText(/Password/i);
+
+  //Get button
+  const submitButton = screen.getByRole("button");
+
+  // Assert elements
+  expect(inputLength).toEqual(1);
+  expect(email).toBeInTheDocument();
+  expect(password).toBeInTheDocument();
+  expect(submitButton.textContent).toBe("OK");
+});
