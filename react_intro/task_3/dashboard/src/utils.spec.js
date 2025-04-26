575aebf4fd3c99e36971ec3a2f7d6d1c1f072e75
khiba-k <khibakoenane@gmail.com>
2025-04-26 15:00:56 +0200

    fix: Used test instead of it

--- react_intro/task_3/dashboard/src/utils.spec.js
+++ react_intro/task_3/dashboard/src/utils.spec.js
@@ -1,7 +1,7 @@
 import { getCurrentYear, getFooterCopy, getLatestNotification } from "./utils";
 
 describe("Utils functions", () => {
-  it("getCurrentYear() returns correct year", () => {
+  test("getCurrentYear() returns correct year", () => {
     // Expected year
     const currentYear = () => {
       const date = new Date();
@@ -12,21 +12,21 @@ describe("Utils functions", () => {
     expect(getCurrentYear()).toBe(currentYear());
   });
 
-  it("getFooterCopy() returns correct string when arg is true", () => {
+  test("getFooterCopy() returns correct string when arg is true", () => {
     const returnString = "Holberton School main dashboard";
 
     // Assert functions return value
     expect(getFooterCopy(false)).toBe(returnString);
   });
 
-  it("getFooterCopy() returns correct string when arg is false", () => {
+  test("getFooterCopy() returns correct string when arg is false", () => {
     const returnString = "Holberton School";
 
     // Assert functions return value
     expect(getFooterCopy(true)).toBe(returnString);
   });
 
-  it("getLatestNotification() returns correct string", () => {
+  test("getLatestNotification() returns correct string", () => {
     const notifyString =
       "<strong>Urgent requirement</strong> - complete by EOD";
 
