b421bbfc0ddb706fb18130d9bea8ca925a3fda9c
khiba-k <khibakoenane@gmail.com>
2025-04-26 10:08:44 +0200

    test: Created test for login form

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -1,59 +1,61 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("h1 element with the text School Dashboard is rendered", () => {
-  render(<App />);
-  const heading = screen.getByRole("heading", { name: /School Dashboard/i });
-  expect(heading).toBeInTheDocument();
-});
+describe("App Component", () => {
+  test("h1 element with the text School Dashboard is rendered", () => {
+    render(<App />);
+    const heading = screen.getByRole("heading", { name: /School Dashboard/i });
+    expect(heading).toBeInTheDocument();
+  });
 
-test("the text content within the 2 p elements in the app-body and app-footer divs matches", () => {
-  render(<App />);
-  const divbody = screen.getByText(/Login to access the full dashboard/i);
-  const divfooter = screen.getByText(/Copyright 2025 - holberton School/i);
+  test("the text content within the 2 p elements in the app-body and app-footer divs matches", () => {
+    render(<App />);
+    const divbody = screen.getByText(/Login to access the full dashboard/i);
+    const divfooter = screen.getByText(/Copyright 2025 - holberton School/i);
 
-  expect(divbody).toBeInTheDocument();
-  expect(divfooter).toBeInTheDocument();
-});
+    expect(divbody).toBeInTheDocument();
+    expect(divfooter).toBeInTheDocument();
+  });
 
-test("an img element is rendered", () => {
-  render(<App />);
-  const image = screen.getByAltText(/holberton logo/i);
-  expect(image).toBeInTheDocument();
-});
+  test("an img element is rendered", () => {
+    render(<App />);
+    const image = screen.getByAltText(/holberton logo/i);
+    expect(image).toBeInTheDocument();
+  });
 
-test("renders two input elements", () => {
-  // Render component
-  render(<App />);
+  test("renders two input elements", () => {
+    // Render component
+    render(<App />);
 
-  //Get input elements
-  const inputElements = screen.getAllByRole("textbox");
-  const inputLength = inputElements.length;
+    //Get input elements
+    const inputElements = screen.getAllByRole("textbox");
+    const inputLength = inputElements.length;
 
-  // Assert element
-  expect(inputLength).toEqual(1);
-});
+    // Assert element
+    expect(inputLength).toEqual(1);
+  });
 
-test("renders two label elements with text 'email' & 'password'", () => {
-  // Render component
-  render(<App />);
+  test("renders two label elements with text 'email' & 'password'", () => {
+    // Render component
+    render(<App />);
 
-  // Get labels
-  const email = screen.getByLabelText(/Email/i);
-  const password = screen.getByLabelText(/Password/i);
+    // Get labels
+    const email = screen.getByLabelText(/Email/i);
+    const password = screen.getByLabelText(/Password/i);
 
-  // Assert elements and values
-  expect(email).toBeInTheDocument();
-  expect(password).toBeInTheDocument();
-});
+    // Assert elements and values
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+  });
 
-test("renders two label elements with text 'emai' & 'password'", () => {
-  // Render component
-  render(<App />);
+  test("renders two button with text 'OK'", () => {
+    // Render component
+    render(<App />);
 
-  //Get button
-  const submitButton = screen.getByRole("button");
+    //Get button
+    const submitButton = screen.getByRole("button");
 
-  // Assert text content of button
-  expect(submitButton.textContent).toBe("OK");
+    // Assert text content of button
+    expect(submitButton.textContent).toBe("OK");
+  });
 });
