60bf8f7142b46321cb49e4a08f4f97e4974c8d1a
khiba-k <khibakoenane@gmail.com>
2025-04-26 14:46:45 +0200

    task: Task 7, create tests for utility functions complete

--- react_intro/task_3/dashboard/src/utils.spec.js
+++ react_intro/task_3/dashboard/src/utils.spec.js
@@ -1,9 +1,7 @@
-import { getCurrentYear, getFooterCopy } from "./utils";
+import { getCurrentYear, getFooterCopy, getLatestNotification } from "./utils";
 
 describe("Utils functions", () => {
   it("getCurrentYear() returns correct year", () => {
-    const returnedYear = getCurrentYear();
-
     // Expected year
     const currentYear = () => {
       const date = new Date();
@@ -11,15 +9,23 @@ describe("Utils functions", () => {
     };
 
     // Assert functions return value
-    expect(returnedYear).toEqual(currentYear());
+    expect(getCurrentYear()).toEqual(currentYear());
   });
 
-  it("getFooterCopy() returns string depend on arg", () => {
+  it("getFooterCopy() returns correct string depending on arg", () => {
     const isFooterCopy = "Holberton School";
     const isNotFooterCopy = "Holberton School main dashboard";
 
     // Assert functions return value
     expect(getFooterCopy(true)).toEqual(isFooterCopy);
     expect(getFooterCopy(false)).toEqual(isNotFooterCopy);
+  });
+
+  it("getLatestNotification() returns correct string", () => {
+    const notifyString =
+      "<strong>Urgent requirement</strong> - complete by EOD";
+
+    // Assert functions return value
+    expect(getLatestNotification()).toEqual(notifyString);
   });
 });
